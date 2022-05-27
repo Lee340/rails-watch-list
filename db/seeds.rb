50cd8ad626513123008fe6460d2f5ac117f2ecf1
Boram Lee <boramlee340@gmail.com>
2022-05-27 10:49:18 +0100

    second challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Movie.destroy_all
+
+20.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraphs,
+    poster_url: Faker::Internet.url,
+    rating: rand(0.0..10.0)
+  )
+  puts "The movie with id:#{movie.id} has been created"
+end
